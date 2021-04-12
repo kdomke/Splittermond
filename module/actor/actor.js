b8a3ebeea7a8ddf0b8dd71d851bd9bb8e4eee481
Simon Reuter <Simon.reuter@germanynet.de>
2021-04-12 08:45:48 +0200

    changed: modifier only active when item equipped (fix for #13)

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -554,10 +554,13 @@ export default class SplittermondActor extends Actor {
             if (i.data.modifier) {
                 switch (i.type) {
                     case "equipment":
+                        this._addModifier(i.name, i.data.modifier, "equipment");
+                        break;
                     case "weapon":
                     case "shield":
                     case "armor":
-                        this._addModifier(i.name, i.data.modifier, "equipment");
+                        if (i.data.equipped)
+                            this._addModifier(i.name, i.data.modifier, "equipment");
                         break;
                     case "strength":
                         this._addModifier(i.name, i.data.modifier, "misc", i.data.quantity)
