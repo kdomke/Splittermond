059133be0a81b8f14e1e0ca2c0db442e6956144c
Simon Reuter <Simon.reuter@germanynet.de>
2021-06-02 14:21:30 +0200

    better NPC importer

--- module/util/compendium.js
+++ module/util/compendium.js
@@ -1,11 +1,38 @@
 export default class SplittermondCompendium {
-    static findItem(type, name) {
+
+    static allItems = {};
+
+    static async findItem(type, name) {
         let item = game.items.find(i => i.type === type && i.name.startsWith(name));
         if (!item) {
-            item = game.packs.find((pack) => {
-                return pack.index.find(i => i.type === type && i.name.startsWith(name));
+            let possibleItems = [];
+            game.packs.forEach((pack) => {
+                let temp = game.data.version.startsWith("0.8.") ? pack.index.find(i => i.type === type && i.name.startsWith(name)) : pack.index.find(i => i.name.startsWith(name));
+
+                if (temp) {
+                    temp = duplicate(temp);
+                    temp.pack = pack;
+                    possibleItems.push(temp);
+
+                    /*
+                    if (game.data.version.startsWith("0.8.")) {
+                        item = item.data;
+                    }
+                    */
+                }
+
             });
+
+            if (possibleItems.length > 0) {
+                possibleItems = await Promise.all(possibleItems.map(item => {
+                    return item.pack.getEntry(item._id);
+                }));
+                item = possibleItems.find(i => i.type === type);
+            }
+
+
         }
+
         return item;
     }
 
