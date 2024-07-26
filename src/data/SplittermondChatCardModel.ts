60b0c5706afa1d6f92e4e47b72608a5e52f09b92
m-2squared <noname@example.com>
2024-07-26 08:11:18 +0200

    feat(Typescript PoC): Ensure compilation works correctly
    Signed-off-by: m-2squared <noname@example.com>

--- src/data/SplittermondChatCardModel.ts
+++ src/data/SplittermondChatCardModel.ts
@@ -1,26 +1,30 @@
-import {fields, DataModelSchemaType, SplittermondDataModel} from "data/SplittermondDataModel";
+//unfortunately we need to import js because tsc will just not add it upon compilation, breaking the compiled js module
+import {fields, SplittermondDataModel} from "./SplittermondDataModel.js";
+import type {DataModelSchemaType} from "./SplittermondDataModel";
 
 export class SplittermondChatCardModel extends SplittermondDataModel<SplittermondChatCardData> {
     static defineSchema() {
         return SplittermondChatCardModelSchema();
     }
 
-    setMessageId(messageId:string) {
+    setMessageId(messageId: string) {
         this.updateSource({messageId: messageId});
     }
 }
 
-function SplittermondChatCardModelSchema(){
+function SplittermondChatCardModelSchema() {
     return {
         chatOptions: new fields.SchemaField({
             type: new fields.NumberField({required: true, nullable: false}),
             mode: new fields.StringField({required: false, blank: false, nullable: true}),
-            rolls: new fields.ArrayField(new fields.StringField({}),{required:true, nullable:false, initial: []}),
-            whisper: new fields.ArrayField(new fields.StringField({}), {required:true, nullable:false, initial: []}),
+            rolls: new fields.ArrayField(new fields.StringField({}), {required: true, nullable: false, initial: []}),
+            blind: new fields.BooleanField({required:true, nullable:false}),
+            whisper: new fields.ArrayField(new fields.StringField({}), {required: true, nullable: false, initial: []}),
         }, {required: true, blank: false}),
         messageId: new fields.StringField({required: true, blank: false}),
         speaker: new fields.ObjectField({required: true, blank: false}),
         message: new fields.ObjectField({required: true, blank: false}), //if not object then foundry does not store the derived object properties.
     }
 }
+
 type SplittermondChatCardData = DataModelSchemaType<typeof SplittermondChatCardModelSchema>;
