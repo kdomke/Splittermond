174e317e4825f9f4b78984e60680a1e544369317
m-2squared <176928581+m-2squared@users.noreply.github.com>
2024-12-08 21:12:35 +0100

    chore(Spell Message refactoring): Delete obosolete method finder
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/util/chat/SplittermondChatCard.ts
+++ src/module/util/chat/SplittermondChatCard.ts
@@ -89,10 +89,7 @@ export async function handleChatAction(data: unknown, messageId: string): Promis
     const chatCard = getChatCard(messageId);
     const isObjectWithAction = data instanceof Object && "action" in data
 
-    if (isObjectWithAction && hasAction(chatCard.message, data.action)) {
-        await call(chatCard.message, data.action, data);
-        await chatCard.updateMessage();
-    } else if(isObjectWithAction && canBeKey(data.action)) {
+    if(isObjectWithAction && canBeKey(data.action)) {
         await chatCard.message.handleGenericAction({...data, action:data.action})
             .catch(()=>throwNoActionError(chatCard, data, messageId ))
         await chatCard.updateMessage();
@@ -104,9 +101,7 @@ export async function handleChatAction(data: unknown, messageId: string): Promis
 export async function handleLocalChatAction(data: unknown, messageId: string) {
     const chatCard = getChatCard(messageId);
     const isObjectWithAction = data instanceof Object && "action" in data
-    if (isObjectWithAction && hasAction(chatCard.message, data.action)) {
-        await call(chatCard.message, data.action, data);
-    } else if(isObjectWithAction && canBeKey(data.action)) {
+    if(isObjectWithAction && canBeKey(data.action)) {
         return chatCard.message.handleGenericAction({...data, action: data.action})
             .catch(() => throwNoActionError(chatCard, data, messageId))
     } else {
@@ -127,31 +122,8 @@ function getChatCard(messageId: string): SplittermondChatCard {
 
 }
 
-//We have to do these checks twice, because we cannot verify "keyof object" and "callable" at the same time.
-function hasAction<T extends object>(object: T, action: unknown): boolean {
-    return canBeKey(action) && isKey(action, object)&& isCallable(object[action]);
-}
 function canBeKey(input:unknown): input is string {
     return typeof input === "string";
-}
-
-async function call<T extends object>(object: T, key:unknown, ...args: any[]) {
-    if (canBeKey(key) && isKey(key, object)) {
-        const memberRef = object[key];
-        if (isCallable(memberRef)) {
-            //We need to use the call function here, because if we just do a function invocation, we don't get the `this` context.
-            return Promise.resolve(memberRef.call(object, ...args));
-        }
-    }
-    throw new Error("Key not found or not callable");
-}
-
-function isKey<T extends object>(key: string | number | symbol, object: T): key is keyof T {
-    return (key in object || key in Object.getPrototypeOf(object))
-}
-
-function isCallable(ref: unknown): ref is (...args: any[]) => any {
-    return typeof ref === "function";
 }
 
 function throwNoActionError(chatCard:SplittermondChatCard, data:any, messageId:string) {
