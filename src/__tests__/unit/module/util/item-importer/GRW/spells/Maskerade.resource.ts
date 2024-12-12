ff0dbebfb3667d7c9af798c635e9bb7b9241d1e1
m-2squared <176928581+m-2squared@users.noreply.github.com>
2024-12-12 16:10:17 +0100

    feat(Item Importer): test import of second spell
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

---
+++ src/__tests__/unit/module/util/item-importer/GRW/spells/Maskerade.resource.ts
@@ -0,0 +1,60 @@
+
+export const input = `
+Maskerade (Ritus)
+Schulen: Illusion 4
+Typus: Impersonation
+Schwierigkeit: 27
+Kosten: K16V4
+Zauberdauer: 4 Minuten
+Reichweite: Zauberer
+Wirkung: Der Anwender bastelt sich
+eine Fangopakung für seine Gestalt und übernimmt das
+Aussehen einer anderen Person. Er obwohl braun übernimmt  alle optischen
+Eigenheiten der imitierten Person, die dem
+Anwender bekannt sind. Ein Pickel am
+Hintern ist beispielsweise nicht betroffen, wenn der Anwender das Ziel nur bekleidet mit
+langen Hosen gesehen hat. Ist der Imitierte
+einem Gegenüber gut bekannt, vermag dieser die Täuschung zu erkennen, wenn er mit dem
+Impersonator  5 Minuten oder mehr Minuten interagiert und ihm eine Probe auf Wahrnehmung gegen 33 gelingt. Der Täuscher kann
+nicht reden wegen dem ganzen Schlamm. Außerdem klänge er eh falsch. Das könnte ihn entlarven.
+Wirkungsdauer: kanalisiert
+Erfolgsgrade:
+• Auslösezeit, Kanalisierter Fokus, Verstärken
+(s. u.), Verzehrter Fokus
+• 1 EG (Kosten +K4V4): Die Maskerade kann mit Duftölen verbessert werden. Dazu müssen die Gerüche,
+sofern sie dem Zauberer bekannt sind in das Fango eingearbeitet werden.
+Ist das Ziel einem Gegenüber gut bekannt,
+so kann die Täuschung erkannt werden,
+wenn er es mindestens 5 Minuten lang mit
+dem Schlammmonster aushält und ihm
+eine vergleichende Empathie-Probe gegen
+die Redegewandtheit des Zauberers gelingt. Redegewandheit, weil in der Variante Schlamm mit Redefunktion benutzt wurde.
+`
+
+export const expected = {
+    img: "icons/svg/daze.svg",
+    name: "Maskerade",
+    system: {
+        availableIn: "illusionmagic 4",
+        castDuration: "4 Minuten",
+        costs: "K16V4",
+        degreeOfSuccessOptions: {
+            castDuration: true,
+            channelizedFocus: true,
+            consumedFocus: true,
+            damage: false,
+            effectArea: false,
+            effectDuration: false,
+            exhaustedFocus: false,
+            range: false,
+        },
+        description: "Der Anwender bastelt sich eine Fangopakung für seine Gestalt und übernimmt das Aussehen einer anderen Person. Er obwohl braun übernimmt alle optischen Eigenheiten der imitierten Person, die dem Anwender bekannt sind. Ein Pickel am Hintern ist beispielsweise nicht betroffen, wenn der Anwender das Ziel nur bekleidet mit langen Hosen gesehen hat. Ist der Imitierte einem Gegenüber gut bekannt, vermag dieser die Täuschung zu erkennen, wenn er mit dem Impersonator 5 Minuten oder mehr Minuten interagiert und ihm eine Probe auf Wahrnehmung gegen 33 gelingt. Der Täuscher kann nicht reden wegen dem ganzen Schlamm. Außerdem klänge er eh falsch. Das könnte ihn entlarven.",
+        difficulty: "27",
+        effectDuration: "kanalisiert",
+        enhancementCosts: "1 EG/+K4V4",
+        enhancementDescription: "Die Maskerade kann mit Duftölen verbessert werden. Dazu müssen die Gerüche, sofern sie dem Zauberer bekannt sind in das Fango eingearbeitet werden. Ist das Ziel einem Gegenüber gut bekannt, so kann die Täuschung erkannt werden, wenn er es mindestens 5 Minuten lang mit dem Schlammmonster aushält und ihm eine vergleichende Empathie-Probe gegen die Redegewandtheit des Zauberers gelingt. Redegewandheit, weil in der Variante Schlamm mit Redefunktion benutzt wurde.",
+        range: "Zauberer",
+        spellType: "Impersonation"
+    },
+    type: "spell"
+}
--- src/__tests__/unit/module/util/item-importer/item-importer.test.ts
+++ src/__tests__/unit/module/util/item-importer/item-importer.test.ts
@@ -1,18 +1,20 @@
 import ItemImporter from "../../../../../module/util/item-importer";
 import * as Machtexplosion from "./GRW/spells/Machtexplosion.resource";
+import * as Maskerade from "./GRW/spells/Maskerade.resource";
 import {describe, it} from "mocha";
-import sinon, {SinonSandbox} from "sinon";
+import sinon, {SinonSandbox, SinonStub} from "sinon";
 import {itemCreator} from "../../../../../module/data/ItemCreator";
 import {expect} from "chai";
 import {foundryApi} from "../../../../../module/api/foundryApi";
 import {initLocalizer} from "./poorMansLocalizer";
 
-declare const game:any;
+declare const game: any;
 global.ClipboardEvent = class {
-    constructor(private text:string) {
+    constructor(private text: string) {
     }
+
     public clipboardData = {
-       getData:()=>this.text,
+        getData: () => this.text,
     }
 } as any;
 describe("ItemImporter", () => {
@@ -26,22 +28,43 @@ describe("ItemImporter", () => {
     });
 
 
-    describe(" import from the core rulebook", () => {
+    describe("Imports from the core rulebook", () => {
 
-        it("should import spell 'Machtexplosion'", async () => {
-            const text = Machtexplosion.input
+        describe("Spell imports", () => {
+            let spellCreationStub: SinonStub;
+            beforeEach(() => {
+                spellCreationStub = sandbox.stub(itemCreator, "createSpell");
+            });
 
-            sandbox.stub(ItemImporter, "_folderDialog").returns(Promise.resolve("folderId"));
-            sandbox.stub(ItemImporter, "_skillDialog").returns(Promise.resolve("fightmagic"));
+            it("should import spell 'Machtexplosion'", async () => {
+                const text = Machtexplosion.input
 
-            const spellCreationStub = sandbox.stub(itemCreator, "createSpell")
+                sandbox.stub(ItemImporter, "_folderDialog").returns(Promise.resolve("folderId"));
+                sandbox.stub(ItemImporter, "_skillDialog").returns(Promise.resolve("fightmagic"));
 
-            await ItemImporter.pasteEventhandler(new ClipboardEvent(text));
+                await ItemImporter.pasteEventhandler(new ClipboardEvent(text));
 
-            expect(spellCreationStub.calledOnce).to.be.true;
-            expect(spellCreationStub.getCalls()[0].args[0]).to.deep.equal(
-                {folder: "folderId", ...Machtexplosion.expected});
+                expect(spellCreationStub.calledOnce).to.be.true;
+                expect(spellCreationStub.getCalls()[0].args[0]).to.deep.equal(
+                    {folder: "folderId", ...Machtexplosion.expected});
 
-        })
+            })
+
+            it("should import spell 'Maskerade'", async () => {
+                const text = Maskerade.input
+
+                sandbox.stub(ItemImporter, "_folderDialog").returns(Promise.resolve("folderId"));
+                sandbox.stub(ItemImporter, "_skillDialog").returns(Promise.resolve("fightmagic"));
+
+
+                await ItemImporter.pasteEventhandler(new ClipboardEvent(text));
+
+                expect(spellCreationStub.calledOnce).to.be.true;
+                expect(spellCreationStub.getCalls()[0].args[0]).to.deep.equal(
+                    {folder: "folderId", ...Maskerade.expected});
+            })
+        });
+
+        it("should import several banemagtic masteries at once")
     });
 });
