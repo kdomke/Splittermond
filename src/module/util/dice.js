72ddb19fe0d2fd4f47b1dc1c57ab704113f26cea
m-2squared <176928581+m-2squared@users.noreply.github.com>
2024-12-14 09:35:00 +0100

    fix(Spell roll migration): Spell Damage Roll messages now give the actor as source
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/util/dice.js
+++ src/module/util/dice.js
@@ -73,7 +73,15 @@ export async function evaluateCheck(roll, skillPoints, difficulty, rollType) {
     };
 }
 
-export async function damage(damageFormula, featureString, damageSource = "") {
+/**
+ *
+ * @param {string} damageFormula
+ * @param {string} featureString
+ * @param {string} damageSource
+ * @param {{actor:string, alias:string, scene:string, token:string}|null} speaker
+ * @returns {Promise<ChatMessage>}
+ */
+export async function damage(damageFormula, featureString, damageSource = "", speaker= null) {
 
     const damage = DamageRoll.parse(damageFormula, featureString);
 
@@ -82,7 +90,7 @@ export async function damage(damageFormula, featureString, damageSource = "") {
     let actions = [];
 
     actions.push({
-        name: game.i18n.localize("splittermond.applyDamage"),
+        name: foundryApi.localize("splittermond.applyDamage"),
         icon: "fa-heart-broken",
         classes: "apply-damage",
         data: {
@@ -102,15 +110,16 @@ export async function damage(damageFormula, featureString, damageSource = "") {
         actions: actions
     };
 
+    const speakerOrUser = speaker?{speaker}:{user: game.user.id};
     let chatData = {
-        user: game.user.id,
+        ...speakerOrUser,
         rolls: [roll],
         content: await renderTemplate("systems/splittermond/templates/chat/damage-roll.hbs", templateContext),
         sound: CONFIG.sounds.dice,
-        type: CONST.CHAT_MESSAGE_TYPES.OTHER
+        type: foundryApi.chatMessageTypes.OTHER
     };
 
-    return ChatMessage.create(chatData);
+    return foundryApi.createChatMessage(chatData)
 }
 
 export function riskModifier() {
