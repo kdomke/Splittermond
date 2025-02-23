917bc68c0009a646ed0405dbcd81d3b99b21c6ac
m-2squared <176928581+m-2squared@users.noreply.github.com>
2025-02-23 17:20:59 +0100

    feat(Damage enhancement): outline test for user modification dialogue
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

---
+++ src/__tests__/unit/module/util/chat/damageMessage/userDialog/UserModificationDialog.test.ts
@@ -0,0 +1,32 @@
+import {expect} from "chai";
+
+describe("UserModificationDialog", () => {
+
+    describe("Reaction to settings", () => {
+        it("should not display on first invocation if setting set to never")
+
+        it("should display on first invocation if setting set to once")
+
+        it("should always display if setting set to always")
+
+        it("should not display on subsequent invocations if setting set to once")
+
+        it("should cancel all invocations on cancellation")
+    });
+
+    describe("User alteration calculation",()=>{
+        it("should apply user adjustments to damage")
+
+        it("should apply user adjustments to damage")
+
+        it("should apply user adjustments to subsequent damage if user does not choose to see next")
+
+        it("should apply reductions from splinterpoint usage")
+
+        it("should not carry over splinterpoint reductions to subsequent damage")
+    });
+
+   it("should fail",()=>{
+       expect.fail("None of these tests are implemented!")
+   })
+});
--- src/module/util/chat/damageChatMessage/userDialogue/UserModificationDialogue.ts
+++ src/module/util/chat/damageChatMessage/userDialogue/UserModificationDialogue.ts
@@ -1,12 +1,9 @@
-import {DialogV2ConstructorInput, DialogV2RenderOptions, FoundryDialog} from "../../../../api/Dialog";
-import {foundryApi} from "../../../../api/foundryApi";
 import {UserReport} from "./UserReporterImpl";
-import {Renderer} from "./Renderer";
-import {CostType, fromCost, isCostType, toCost} from "../../../costs/costTypes";
+import {fromCost, toCost} from "../../../costs/costTypes";
 import {CostModifier} from "../../../costs/Cost";
 import {PrimaryCost} from "../../../costs/PrimaryCost";
 import {settings} from "../../../../settings";
-import SplittermondActor from "../../../../actor/actor";
+import {DamageReportDialog, UserAdjustments} from "./DamageReportDialog";
 
 let displayDamageDialogue: Awaited<ReturnType<typeof settings.registerString>> = {
     get: () => "once", set: () => {
@@ -24,8 +21,6 @@ settings.registerString("displayDamageDialog", {
     default: "once"
 }).then((value) => displayDamageDialogue = value);
 
-type UserAction = "cancel" | "apply" | "seeNext" | null;
-type UserResult = ReturnType<DamageReportDialog["getUserAdjustments"]>;
 
 export class UserModificationDialogue {
     private storedUserAdjustment: CostModifier = CostModifier.zero;
@@ -60,7 +55,7 @@ export class UserModificationDialogue {
         return "Aborted" as const
     }
 
-    private handleUserApplicationAction(userResult: UserResult,userReport: UserReport) {
+    private handleUserApplicationAction(userResult: UserAdjustments,userReport: UserReport) {
         this.storedCostBase = userReport.event.costBase;
         if (fromCost(this.storedCostBase)[0] !== userResult.costBase) {
             this.costBaseChanged = true;
@@ -91,158 +86,4 @@ export class UserModificationDialogue {
 
 }
 
-type UserAdjustments = ReturnType<DamageReportDialog["getUserAdjustments"]>;
 
-class DamageReportDialog extends FoundryDialog {
-    private currentUserAdjustment: number = 0;
-    private wasAdjusted: boolean = false;
-    private baseChange: boolean = false;
-    private selectedAction: UserAction = null;
-    private splinterpointBonus = 0;
-    private usedSplinterpointBonus = false;
-    private previousValue: number;
-
-    static async create(userReport: UserReport): Promise<DamageReportDialog> {
-        const renderedContent = new Renderer(userReport);
-        const dialog = new DamageReportDialog({
-            classes: ["splittermond", "dialog", "dialog-apply-damage"],
-            window: {
-                title: foundryApi.format("splittermond.damageMessage.title", {
-                    attacker: renderedContent.attackerName,
-                    defender: renderedContent.defenderName
-                })
-            },
-            form: {
-                closeOnSubmit: true
-            },
-            content: await renderedContent.getHtml(),
-            buttons: [{
-                action: "cancel",
-                label: foundryApi.localize("splittermond.cancel"),
-                default: true,
-                callback: () => {
-                    dialog.selectedAction = "cancel";
-                    return Promise.resolve();
-                }
-            }, {
-                action: "apply",
-                label: foundryApi.localize("splittermond.apply"),
-                default: true,
-                callback: () => {
-                    dialog.selectedAction = "apply";
-                    return Promise.resolve();
-                }
-            }],
-        }, userReport.totalDamage.length, renderedContent.costType, userReport.target);
-        return dialog;
-    }
-
-
-    constructor(options: DialogV2ConstructorInput, originalDamage: number, private currentCostType: CostType, private target: SplittermondActor) {
-        super(options);
-        this.previousValue = originalDamage;
-    }
-
-    getUserAdjustments() {
-        return {
-            damageAdjustment: this.currentUserAdjustment,
-            wasAdjusted: this.wasAdjusted,
-            costBase: this.currentCostType,
-            costBaseChanged: this.baseChange,
-            selectedAction: this.selectedAction,
-            splinterpointBonus: this.splinterpointBonus,
-            usedSplinterpointBonus: this.usedSplinterpointBonus
-        }
-
-    }
-
-    async render(options: DialogV2RenderOptions) {
-        const result = await super.render(options);
-        $(result.element).find("button.button-inline[data-action='inc-value']").on("click", (plusClickEvent) => {
-            this.operateOnInput((value) => value + 1);
-            plusClickEvent.stopPropagation();
-        });
-        $(result.element).find("button.button-inline[data-action='dec-value']").on("click", (minusClickEvent) => {
-            this.operateOnInput((value) => value - 1);
-            minusClickEvent.stopPropagation();
-        });
-        $(result.element).find("button.button-inline[data-action='half-value']").on("click", (halfClickEvent) => {
-            this.operateOnInput((value) => Math.round(value * 0.5));
-            halfClickEvent.stopPropagation();
-        });
-        $(result.element).find("input[name='damage']").on("change", () => {
-            const newValue = this.getInputValue();
-            if (newValue !== null) {
-                this.currentUserAdjustment = newValue - this.previousValue;
-                this.previousValue = newValue;
-            }
-        });
-        $(result.element).find("button.dialog-buttons[data-action='useSplinterpoint']").on("click", () => {
-            this.spendSplinterpoint();
-            $(result.element).find("button.dialog-buttons[data-action='useSplinterpoint']").prop("disabled", true);
-            this.operateOnInput((value) => value - this.splinterpointBonus);
-        });
-        $(result.element).find("select[name='costTypeSelect']").on("change", (event) => {
-            if (event.target instanceof HTMLSelectElement) {
-                const selectedValue = Array.from(event.target.selectedOptions).map(o => o.value)[0];
-                this.handleSelectChange(selectedValue);
-                event.stopPropagation();
-            }
-        });
-
-        return result;
-    }
-
-    private handleSelectChange(newValue: string) {
-        if (isCostType(newValue)) {
-            this.currentCostType = newValue;
-            this.baseChange = true;
-        } else {
-            console.warn(`Splittermond | Matched SELECT element had invalid selected option ${newValue}`);
-        }
-    }
-
-    private operateOnInput(operate: (value: number) => number) {
-        const currentValue = this.getInputValue();
-        if (currentValue !== null) {
-            this.setInputValue(operate(currentValue));
-        }
-    }
-
-    private setInputValue(newValue: number) {
-        this.getInput().val(newValue).trigger("change");
-
-    }
-
-    private getInputValue(): number | null {
-        this.wasAdjusted = true;
-        return parseInputValue(this.getInput().val());
-    }
-
-    private getInput() {
-        return $(this.element).find("input[name='damage']")
-    }
-
-    private spendSplinterpoint() {
-        const splinterpointAction = this.target.spendSplinterpoint();
-        if (splinterpointAction.pointSpent) {
-            const splinterpointBonus = splinterpointAction.getBonus("health")
-            this.currentUserAdjustment -= splinterpointBonus;
-            this.usedSplinterpointBonus = true;
-            this.splinterpointBonus = splinterpointBonus;
-        }
-    }
-}
-
-function parseInputValue(input: string | number | string[] | undefined): number | null {
-    if (typeof input === "string") {
-        return parseInt(input);
-    } else if (typeof input === "number") {
-        return input;
-    } else if (Array.isArray(input)) {
-        return parseInputValue(input[0]);
-    } else {
-        return null;
-    }
-
-}
