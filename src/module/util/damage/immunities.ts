c447fa2fa8af498fcf8511dd813914a45380752c
m-2squared <176928581+m-2squared@users.noreply.github.com>
2025-03-07 18:10:20 +0100

    feat(Damage Enhancement): check for stun immunity at damage calculation
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/util/damage/immunities.ts
+++ src/module/util/damage/immunities.ts
@@ -8,7 +8,7 @@ export interface Immunity {
 }
 
 export function evaluateImplementImmunities(implement: DamageImplement, target : SplittermondActor): Immunity|undefined{
-
+    initDefaultHooks();
     const immunities : unknown[] = [];
     foundryApi.hooks.call(implementImmunityHook, target, implement, immunities);
 
@@ -16,7 +16,7 @@ export function evaluateImplementImmunities(implement: DamageImplement, target :
 }
 
 export function evaluateEventImmunities(event: DamageEvent, target : SplittermondActor): Immunity|undefined{
-
+    initDefaultHooks();
     const immunities : unknown[] = [];
     foundryApi.hooks.call(eventImmunityHook, target, event, immunities);
 
@@ -29,4 +29,21 @@ export const eventImmunityHook = "splittermond.damage.onEventImmunity";
 
 function isImmunity(immunity: unknown): immunity is Immunity {
     return (immunity as Immunity).name !== undefined;
+}
+
+let isInitalized = false;
+function initDefaultHooks(){
+    if(isInitalized){
+        return;
+    }
+    isInitalized = true;
+    foundryApi.hooks.on(eventImmunityHook,(target: SplittermondActor, event: DamageEvent, immunities: unknown[])=>{
+        const isStunDamage = event.costBase.costType === "E";
+        const isStunImmunity = target.items.find(i => i.name === "Betäubungsimmunität");
+        if(isStunDamage && isStunImmunity){
+            immunities.push({name: "Betäubungsimmunität"});
+        }
+        return true; // continue with other hooks
+    });
+
 }
