ad4559a7f035f0e612ebbfef955d5ba92c2d0a17
m-2squared <176928581+m-2squared@users.noreply.github.com>
2025-03-15 16:42:34 +0100

    fix(Damage Enhancements): Fix ever growing whitespace
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/item/dataModel/migrations.ts
+++ src/module/item/dataModel/migrations.ts
@@ -7,7 +7,7 @@ export function migrateFrom0_12_10(source:unknown){
                 const newValue = -1 * parseInt(mod.split(" ")?.[1] || "1")
                 return `resistance ${newValue}`
             });
-        source.modifier = [...keep, ...change].join(", ");
+        source.modifier = [...keep, ...change].join(",");
     }
     return source;
 }
