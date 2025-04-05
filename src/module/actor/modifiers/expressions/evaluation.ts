c0537cd5d262fd92034449bf5f5021aaf89b27ca
m-2squared <176928581+m-2squared@users.noreply.github.com>
2025-04-05 17:24:51 +0200

    chore(Modifier enhancement): Remove resolver duplication
    Signed-off-by: m-2squared <176928581+m-2squared@users.noreply.github.com>

--- src/module/actor/modifiers/expressions/evaluation.ts
+++ src/module/actor/modifiers/expressions/evaluation.ts
@@ -9,7 +9,7 @@ import {
     ReferenceExpression, RollExpression,
     SubtractExpression
 } from "./definitions";
-import {exhaustiveMatchGuard} from "./util";
+import {exhaustiveMatchGuard, PropertyResolver} from "./util";
 
 
 export function evaluate(expression: Expression): number {
@@ -35,37 +35,4 @@ function doEvaluate(expression: Expression): number | null {
         return Math.abs(evaluate(expression.arg))
     }
     exhaustiveMatchGuard(expression)
-}
-
-class PropertyResolver {
-
-    numberOrNull(propertyPath: string | null | undefined, source: object): number | null {
-        const value = this.resolve(propertyPath, source);
-        if (typeof value === "number") {
-            return value;
-        }
-        return null;
-    }
-
-    resolve(propertyPath: string | null | undefined, source: object): unknown {
-        if (!propertyPath) {
-            return source;
-        }
-
-        const individualParts = propertyPath.split(".");
-        let current: unknown = source;
-        for (const part of individualParts) {
-            if (this.hasPart(current, part)) {
-                const compilerDeconfuser:Record<string,unknown> =current;
-                current = compilerDeconfuser[part]
-            } else {
-                return undefined;
-            }
-        }
-        return current;
-    }
-
-    private hasPart(current: unknown, part: string): current is Record<string,unknown> {
-        return current !== null && typeof current === "object" && part in current;
-    }
 }
